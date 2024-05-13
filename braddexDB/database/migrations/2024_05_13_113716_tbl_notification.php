985099544f33d66a34e953476b04b8f12bf15ebe
CedAlegsao <cedrickcuencaalegsao@gmail.com>
2024-05-13 19:51:14 +0800

    code

---
+++ braddexDB/database/migrations/2024_05_13_113716_tbl_notification.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_notif', function (Blueprint $table) {
+            $table->id();
+            $table->string('onwed_by', 10)->nullable();
+            $table->string('from', 50)->nullable();
+            $table->string('title', 15)->nullable();
+            $table->string('act_details', 255)->nullable();
+            $table->boolean('read')->nullable();
+            $table->boolean('admin_only')->nullable();
+            $table->string('created_at', 20)->nullable();
+            $table->string('updated_at', 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_notif');
+    }
+};
