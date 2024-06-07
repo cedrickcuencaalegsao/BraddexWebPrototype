4e4071842b93cf3ec43a4c8461d9daed3de0a1c1
CedAlegsao <cedrickcuencaalegsao@gmail.com>
2024-06-07 18:49:11 +0800

    code

--- src/components/EditCartComponents/CartRightTop/CartRightTop.jsx
+++ src/components/EditCartComponents/CartRightTop/CartRightTop.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
 import "./CartRightTop.scss";
+import { useEffect, useState } from "react";
+import moment from "moment";
+import Switch from "@mui/material/Switch";
 
 const EditCartRightTop = (data) => {
   const [cartData, setCartData] = useState([]);
@@ -10,6 +12,23 @@ const EditCartRightTop = (data) => {
     }
   }, [data]);
 
+  const formattedDate = (data) => {
+    let date;
+    if (data !== null) {
+      date = moment(data).format("YYYY-MM-DD");
+      return date;
+    }
+    return "No Date.";
+  };
+
+  const checked = (data) => {
+    if (data !== 0) {
+      return true;
+    }
+  };
+
+  console.log(cartData);
+
   return (
     <div className="edit-cart-right-top">
       <div className="title-wrapper">
@@ -18,7 +37,7 @@ const EditCartRightTop = (data) => {
       <div className="cart-details-wrapper">
         {cartData.length === 0 ? (
           <div className="loading-wrapper">
-            <h1 className="loading">Loading...</h1>
+            <span className="loading">Loading...</span>
           </div>
         ) : (
           cartData.map((item) => (
@@ -54,8 +73,31 @@ const EditCartRightTop = (data) => {
               </div>
               <div className="right-cart-details">
                 <div className="items">
-                  <span className="indicator"></span>
-                  <input type="text" className="value" />
+                  <span className="indicator">Date Created:</span>
+                  <input
+                    type={item.created_at === null ? "date" : "text"}
+                    className="value"
+                    disabled="true"
+                    value={formattedDate(item.created_at)}
+                  />
+                </div>
+                <div className="items">
+                  <span className="indicator">Date Updated:</span>
+                  <input
+                    type={item.created_at === null ? "date" : "text"}
+                    className="value"
+                    disabled="true"
+                    value={formattedDate(item.updated_at)}
+                  />
+                </div>
+                <div className="items">
+                  <span className="indicator">
+                    Mark as deleted or not deleted
+                  </span>
+                  <Switch
+                    className="switch"
+                    checked={checked(item.isDeleted)}
+                  />
                 </div>
               </div>
             </div>
