a02ddb1dfa2a206abb67b92e526f2332273ecc1a
CedAlegsao <cedrickcuencaalegsao@gmail.com>
2024-06-19 10:22:45 +0800

    code

--- src/components/EditOrderComponents/BottomLeft/BottomLeft.jsx
+++ src/components/EditOrderComponents/BottomLeft/BottomLeft.jsx
@@ -1,3 +1,4 @@
+import "./BottomLeft.scss";
 import { useEffect, useState } from "react";
 
 const EditOrderBottomLeft = (data) => {
@@ -20,29 +21,47 @@ const EditOrderBottomLeft = (data) => {
             <spam className="title">Order summary</spam>
           </div>
           <div className="details-wrapper">
-            <div className="order-id-wrapper">
-              <span className="indicator">order id </span>
-              <span className="value">{item.orderID}</span>
+            <div className="left-order-details-wrapper">
+              <div className="order-id-wrapper">
+                <span className="indicator">order id </span>
+                <span className="value">{item.orderID}</span>
+              </div>
+              <div className="owner-wrapper">
+                <span className="indicator">owner id </span>
+                <span className="value">{item.userID}</span>
+              </div>
+              <div className="payment-type-wrapper">
+                <span className="indicator">payment type </span>
+                <span className="value">{item.paymentType}</span>
+              </div>
+              <div className="quantity-wrapper">
+                <span className="indicator">quantity </span>
+                <span className="value">{item.quantity}</span>
+              </div>
             </div>
-            <div className="owner-wrapper">
-              <span className="indicator">owner id </span>
-              <span className="value">{item.userID}</span>
-            </div>
-            <div className="payment-type-wrapper">
-              <span className="indicator">payment type </span>
-              <span className="value">{item.paymentType}</span>
-            </div>
-            <div className="quantity-wrapper">
-              <span className="indicator">quantity </span>
-              <span className="value">{item.quantity}</span>
-            </div>
-            <div className="delivery-address-wrapper">
-              <span className="indicator">delivery address </span>
-              <span className="value">{item.userAddress}</span>
-            </div>
-            <div className="cancelled-status-wrapper">
-              <span className="indicator">Cancelled</span>
-              
+            <div className="right-order-details-wrapper">
+              <div className="delivery-address-wrapper">
+                <span className="indicator">delivery address </span>
+                <span className="value">{item.userAddress}</span>
+              </div>
+              <div className="cancelled-status-wrapper">
+                <span className="indicator">Cancelled </span>
+                <span className="value">
+                  {item.isCancelled === 1 ? "True" : "False"}
+                </span>
+              </div>
+              <div className="paid-status-wrapper">
+                <span className="indicator">Paid </span>
+                <span className="value">
+                  {item.isPaid === 1 ? "True" : "False"}
+                </span>
+              </div>
+              <div className="delivered-status-wrapper">
+                <span className="indicator">Delivered </span>
+                <spam className="value">
+                  {item.isDelivered === 1 ? "True" : "False"}
+                </spam>
+              </div>
             </div>
           </div>
         </div>
