7b3f0b48c06ebbc43b7ca56f893208ca49688d60
CedAlegsao <cedrickcuencaalegsao@gmail.com>
2024-06-29 10:49:40 +0800

    code update admin profile picture done here.

--- src/components/AdminProfileComponents/ButtomRight/BottomRight.jsx
+++ src/components/AdminProfileComponents/ButtomRight/BottomRight.jsx
@@ -3,9 +3,13 @@ import LinearProgress from "@mui/material/LinearProgress";
 import { useEffect, useState } from "react";
 import SaveIcon from "@mui/icons-material/Save";
 import AddAPhotoOutlinedIcon from "@mui/icons-material/AddAPhotoOutlined";
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const EditAdminBottomRight = (args) => {
   const data = args.data;
+  const uuid = localStorage.getItem("uuid");
+  const history = useHistory();
   const [userData, setUserData] = useState({});
   const [loading, setLoading] = useState(true);
   const [profilePicture, setProfilePicture] = useState("");
@@ -32,7 +36,37 @@ const EditAdminBottomRight = (args) => {
     }
   };
 
-  console.log(userData);
+  const saveProfileAPI = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("uuid", uuid);
+      formData.append("image", profilePicture);
+      const API = await axios.post(
+        "http://127.0.0.1:8000/api/updateprofilepicture",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const saveProfilePic = async (ev) => {
+    ev.preventDefault();
+    console.log(uuid);
+    try {
+      const response = await saveProfileAPI();
+      response && history.push("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="edit-admin-bottom-right">
@@ -46,7 +80,7 @@ const EditAdminBottomRight = (args) => {
             <input type="file" onChange={previewPicture} />
           </div>
           <div className="btn-save-wrapper">
-            <SaveIcon className="btn-save" />
+            <SaveIcon className="btn-save" onClick={saveProfilePic} />
           </div>
         </div>
       </div>
@@ -68,8 +102,10 @@ const EditAdminBottomRight = (args) => {
         <div className="image-preview">
           {preview ? (
             <img src={preview} alt="preview" />
+          ) : userData.image !== null ? (
+            <span>No preview available.</span>
           ) : (
-            <span>No preview available</span>
+            <span>No preview available. Please update profile picture</span>
           )}
         </div>
       </div>
